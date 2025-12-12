8151c7ca8ca6cedb7173824a337980eb91af6308
te <thrzl@icloud.com>
2025-12-12 18:58:29 -0500

    add button to login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,25 +2,6 @@
     import { invoke } from "@tauri-apps/api/core";
     import { onMount } from "svelte";
 
-    onMount(async () => {
-        if (await invoke("vault_exists")) {
-            (
-                document.querySelector("input") as HTMLInputElement
-            ).addEventListener("keypress", async (event) => {
-                if (event.key !== "Enter") return;
-                const error = await invoke("load_vault", {
-                    password: (
-                        document.querySelector("input") as HTMLInputElement
-                    ).value,
-                });
-                if (error) return console.log(error);
-                window.location.href = "/home";
-            });
-        } else {
-            window.location.href = "/create-vault";
-        }
-    });
-
     type Key = {
         id: string;
         name: string;
@@ -32,17 +13,32 @@
 
     let error = $state("");
 
+    onMount(async () => {
+        if (await invoke("vault_exists")) {
+            (
+                document.querySelector("input") as HTMLInputElement
+            ).addEventListener("keypress", async (event) => {
+                if (event.key !== "Enter") return;
+                const invokeError: string = await invoke("load_vault", {
+                    password: (
+                        document.querySelector("input") as HTMLInputElement
+                    ).value,
+                });
+                error = invokeError || "";
+                window.location.href = "/home";
+            });
+        } else {
+            window.location.href = "/create-vault";
+        }
+    });
     async function unlockVault(event: Event) {
         event.preventDefault();
-        if (!passwordInput) {
-            error = "password input field could not be found";
-            return;
-        }
-        error = await invoke("unlock_vault", { password: passwordInput.value });
-        if (!error) {
-            passwordInput.value = "";
-            window.location.href = "/home";
-        }
+        const invokeError: string = await invoke("load_vault", {
+            password: (document.querySelector("input") as HTMLInputElement)
+                .value,
+        });
+        error = invokeError || "";
+        window.location.href = "/home";
     }
 
     // let isFirstOpenRes: Promise<boolean> = invoke("is_first_open");
@@ -51,4 +47,6 @@
 <main class="container">
     <p>welcome back :P</p>
     <input type="password" placeholder="enter your vault password" />
+    <button type="submit" onclick={unlockVault}>unlock vault</button>
+    <p style:color="red">{error}</p>
 </main>
