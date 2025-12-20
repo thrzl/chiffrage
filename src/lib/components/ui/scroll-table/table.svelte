a421d00a7b1f12684e8b50aac69e3b06691434b5
te <thrzl@icloud.com>
2025-12-19 20:30:15 -0500

    redo encrypt page

---
+++ src/lib/components/ui/scroll-table/table.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { HTMLTableAttributes } from "svelte/elements";
+	import { cn, type WithElementRef } from "$lib/utils.js";
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		height = "auto",
+		children,
+		...restProps
+	}: WithElementRef<HTMLTableAttributes> & {height: string} = $props();
+</script>
+
+<div data-slot="table-container" class="relative w-full overflow-y-auto" style={`height: ${height}`}>
+	<table
+		bind:this={ref}
+		data-slot="table"
+		class={cn("w-full caption-bottom text-sm", className)}
+		{...restProps}
+	>
+		{@render children?.()}
+	</table>
+</div>
--- src/routes/encrypt/+page.svelte
+++ src/routes/encrypt/+page.svelte
@@ -3,21 +3,31 @@
     import { open } from "@tauri-apps/plugin-dialog";
     import type { Key, Progress } from "$lib/main";
     import {getFileName} from "$lib/main"
-    import * as Table from "$lib/components/ui/table/index";
-    import * as Empty from "$lib/components/ui/empty/index";
+    import * as Table from "$lib/components/ui/scroll-table/index";
+    import * as Alert from "$lib/components/ui/alert/index";
+    import Label from "$lib/components/ui/label/label.svelte";
+    import {ScrollArea} from "$lib/components/ui/scroll-area/index";
+    import {animate} from "motion/mini"
+    import { TrashIcon, TriangleAlert } from "@lucide/svelte";
+    import {andList} from "human-list";
+    import * as Select from "$lib/components/ui/select/index.js";
+    import Button from "$lib/components/ui/button/button.svelte";
 
     let error = $state("");
     let message = $state("");
     let progress: Progress | null = $state(null);
-    let key = $state("");
+    let chosenKeys: string[] = $state([]);
     let files: string[] | null = $state(null);
+
+    let alertElement: HTMLDivElement | undefined = $state();
 
     async function chooseFile(event: Event) {
         event.preventDefault();
-        files = await open({
+        let newFiles = await open({
             multiple: true,
             directory: false,
         });
+        if (newFiles) files = [...new Set([...(files || []), ...newFiles])]
     }
     async function encryptFile(event: Event) {
         event.preventDefault();
@@ -25,7 +35,7 @@
             error = "no file selected";
             return;
         }
-        if (!key) {
+        if (chosenKeys.length === 0) {
             error = "no key selected";
             return;
         }
@@ -40,62 +50,109 @@
             message = "";
         };
         error = await invoke("encrypt_file_cmd", {
-            publicKeys: [key],
+            publicKeys: chosenKeys,
             reader: channel,
             files,
         });
     }
-    let keysFetch: Promise<Key[]> = $state(invoke("fetch_keys"));
+    let keys: Key[] = $state(await invoke("fetch_keys"));
+    let privateKeys = keys.filter(key => key.key_type === "Private");
+    let publicKeys = keys.filter(key => key.key_type === "Public");
+    let keyMap = $derived(Object.fromEntries(keys.map(key => ([key.id, key]))));
+    function updateAlert() {
+      if (!alertElement) return
+      let shouldAlert = !(chosenKeys.length === 0 || chosenKeys.some(id => keyMap[id].key_type === "Private"));
+        animate(alertElement, {
+          height: shouldAlert ? `${alertElement.scrollHeight}px` : 0
+        }, { duration: 0.2, ease: "easeOut" }).then(() => alertElement!.style.marginTop = shouldAlert ? "0.5rem" : "0")
+    }
     // listen("update-keys", () => (keysFetch = invoke("fetch_keys")));
 </script>
 
 <main class="container">
-    <h1>encrypt to key</h1>
+    <h1 class="text-2xl font-bold mb-2">encrypt to key</h1>
 
-    <form onsubmit={chooseFile}>
-        <select bind:value={key}>
-            {#await keysFetch}
-                <option value="" disabled selected>loading keys</option>
-            {:then keys}
-                <option value="" disabled selected>choose a key</option>
-                {#if keys}
-                    {#each keys as key}
-                        <option value={key.id}>{key.name}</option>
-                    {/each}
-                {:else}
-                    <option value="no-key" disabled>no keys!</option>
-                {/if}
-            {/await}
-        </select>
-        <button onclick={chooseFile}
+    <form onsubmit={chooseFile} class="w-3/4 mx-auto">
+        <div class="flex flex-row gap-2 justify-items-center justify-center mx-auto">
+        <Select.Root type="multiple" name="target keys" bind:value={chosenKeys} onValueChange={updateAlert}>
+          <Select.Trigger class="w-[180px] flex-grow">
+            <p>{@html chosenKeys.length > 0 ? andList(chosenKeys.map(id => `<span class="font-bold">${keyMap[id].name}</span>`)) : "choose keys..."}</p>
+          </Select.Trigger>
+          <Select.Content>
+            {#if publicKeys.length > 0}
+            <Select.Group>
+              <Select.Label>public keys</Select.Label>
+              {#each publicKeys as key}
+                <Select.Item
+                  value={key.id}
+                  label={key.name}
+                >
+                  {key.name}
+                </Select.Item>
+              {/each}
+            </Select.Group>
+            {/if}
+            {#if privateKeys.length > 0}
+                <Select.Group>
+              <Select.Label>private keys</Select.Label>
+              {#each privateKeys as key}
+                <Select.Item
+                  value={key.id}
+                  label={key.name}
+                >
+                  {key.name}
+                </Select.Item>
+              {/each}
+            </Select.Group>
+            {/if}
+            {#if publicKeys.length < 0 && privateKeys.length < 0}
+                <Select.Item
+                  value={"#"}
+                  label={"no key"}
+                  disabled
+                >
+                  {"no keys"}
+                </Select.Item>
+            {/if}
+          </Select.Content>
+        </Select.Root>
+        <Button onclick={chooseFile} variant={"secondary"}
             >{files
                 ? `${files.length} files selected`
-                : "choose file(s)"}</button
+                : "choose file(s)"}</Button
         >
-        <button
+        </div>
+        <div bind:this={alertElement} class="text-left overflow-hidden mt-0 h-0">
+        <Alert.Root>
+            <TriangleAlert />
+            <Alert.Title>consider adding a private key</Alert.Title>
+            <Alert.Description>without encrypting to one of your own keys, it will not be possible for you to decrypt it later</Alert.Description>
+        </Alert.Root></div>
+        <Button
             onclick={encryptFile}
-            style:width="75%"
-            style:margin="2rem"
-            style:margin-top="0.5rem">encrypt</button
+            disabled={chosenKeys.length === 0 || !files}
+            class="w-full mt-2">encrypt</Button
         >
     </form>
-    <table style="text-align: left; margin: 2rem">
-        <thead>
-            <tr>
-                <th>name</th>
-                <th>extension</th>
-                <th>remove</th>
-            </tr>
-        </thead>
-        <tbody>
+    <div class="w-3/4 mx-auto mt-4">
+    <Label for="selected-files" class="mb-2">selected files</Label>
+    <Table.Root height="12rem" id="selected-files" class="table-fixed">
+        <Table.Header>
+            <Table.Row>
+                <Table.Head class="pl-4 sticky top-0 bg-secondary w-4/5">name</Table.Head>
+                <Table.Head class="pl-4 sticky top-0 bg-secondary w-1/5">remove</Table.Head>
+            </Table.Row>
+        </Table.Header>
+        <Table.Body>
             {#each files as file}
-                <tr>
-                    <td>{getFileName(file)}</td>
-                    <td>{file.split(".").slice(-1)}</td>
-                    <td class="delete-button" onclick={() => files = files!.length > 1 ? files!.filter((f) => f !== file) : null}>x</td>
-                </tr>{/each}
-        </tbody>
-    </table>
+                <Table.Row>
+                    <Table.Cell class="truncate">{getFileName(file)}</Table.Cell>
+                    <Table.Cell><Button variant={"secondary"} class="cursor-pointer" onclick={() => files = files!.length > 1 ? files!.filter((f) => f !== file) : null}><TrashIcon class="w-4"/></Button></Table.Cell>
+                </Table.Row>{/each}
+        </Table.Body>
+    </Table.Root>
+        <!-- </ScrollArea> -->
+    </div>
     <div
         style="background-color: green; height: 10px"
         style:width={progress
@@ -107,15 +164,4 @@
 </main>
 
 <style>
-    .delete-button {
-        font-family: monospace;
-        text-align: center;
-        font-size: 1.5rem
-    }
-    .delete-button:hover {
-        cursor: pointer;
-    }
-    td {
-        word-break: break-all;
-    }
 </style>
