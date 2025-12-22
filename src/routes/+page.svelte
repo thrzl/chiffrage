a3ecb8ad4bddfe9c036f5887b762a6d0d166c1a6
te <thrzl@icloud.com>
2025-12-21 23:24:54 -0500

    feat: change buttons on keys page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,63 +1,60 @@
 <script lang="ts">
     import { invoke } from "@tauri-apps/api/core";
     import { listen } from "@tauri-apps/api/event";
-    import { openWindow } from "$lib/main";
     import type { Key } from "$lib/main";
     import { Button } from "$lib/components/ui/button/index";
     import * as Table from "$lib/components/ui/scroll-table/index";
     import * as Empty from "$lib/components/ui/empty/index";
-    import Plus from "@lucide/svelte/icons/plus";
-    import Lock from "@lucide/svelte/icons/lock";
-    import Unlock from "@lucide/svelte/icons/lock-open";
     import KeyIcon from "@lucide/svelte/icons/key-round";
-    import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index";
     import KeyImportDialog from "../components/KeyImportDialog.svelte";
     import KeyGenDialog from "../components/KeyGenDialog.svelte";
     import KeyViewDialog from "../components/KeyViewDialog.svelte";
+    import { RefreshCcwDotIcon, FolderKeyIcon } from "@lucide/svelte";
 
     let keygenDialogOpen = $state(false);
     let keyImportDialogOpen = $state(false);
-    let selectedKey: Key | undefined = $state(undefined)
+    let selectedKey: Key | undefined = $state(undefined);
     if (!(await invoke("vault_exists"))) {
         window.location.href = "/create-vault";
     }
     let keys: Key[] = $state(await invoke("fetch_keys"));
-    listen("update-keys", async () => {keys = await invoke("fetch_keys"); selectedKey = undefined});
+    listen("update-keys", async () => {
+        keys = await invoke("fetch_keys");
+        selectedKey = undefined;
+    });
 </script>
 
 <main class="container">
-    <h1 class="text-4xl font-bold mb-8">your keys</h1>
+    <h1 class="text-2xl font-bold mb-2">your keys</h1>
     {#if keys.length > 0}
-    <nav>
-        <Button onclick={() => openWindow("encrypt", "encrypt")}
-            ><Lock /> encrypt</Button
+        <nav>
+            <Button
+                variant={"secondary"}
+                onclick={() => (keygenDialogOpen = true)}
+                ><RefreshCcwDotIcon /> generate key</Button
+            >
+            <Button
+                variant={"secondary"}
+                onclick={() => (keyImportDialogOpen = true)}
+                ><FolderKeyIcon /> import key</Button
+            >
+        </nav>
+        <Table.Root
+            height={"16rem"}
+            style="text-align: left; max-width: 100vw"
+            containerClass="m-2"
         >
-        <Button onclick={() => openWindow("decrypt", "decrypt")}
-            ><Unlock /> decrypt</Button
-        >
-        <DropdownMenu.Root>
-          <DropdownMenu.Trigger>
-            {#snippet child({ props })}
-              <Button {...props}><Plus />new key</Button>
-            {/snippet}
-          </DropdownMenu.Trigger>
-          <DropdownMenu.Content class="w-56" align="start">
-              <DropdownMenu.Item onSelect={() => keygenDialogOpen = true}>
-                  generate key
-                <!-- <DropdownMenu.Shortcut>⇧⌘P</DropdownMenu.Shortcut> -->
-              </DropdownMenu.Item>
-              <DropdownMenu.Item onSelect={() => keyImportDialogOpen = true}>
-                  import key
-              </DropdownMenu.Item>
-          </DropdownMenu.Content>
-        </DropdownMenu.Root>
-    </nav>
-        <Table.Root height={"16rem"} style="text-align: left; max-width: 100vw" containerClass="m-8">
             <Table.Header>
                 <Table.Row>
-                    <Table.Head class="sticky top-0 bg-secondary">type</Table.Head>
-                    <Table.Head class="sticky top-0 bg-secondary">name</Table.Head>
-                    <Table.Head class="sticky top-0 bg-secondary">date created</Table.Head>
+                    <Table.Head class="sticky top-0 bg-secondary"
+                        >type</Table.Head
+                    >
+                    <Table.Head class="sticky top-0 bg-secondary"
+                        >name</Table.Head
+                    >
+                    <Table.Head class="sticky top-0 bg-secondary"
+                        >date created</Table.Head
+                    >
                 </Table.Row>
             </Table.Header>
             <Table.Body>
@@ -66,8 +63,9 @@
                         class="cursor-pointer"
                         onclick={() => {
                             {
-                              console.log(`selected key: ${key.id}`);
-                              selectedKey = key};
+                                console.log(`selected key: ${key.id}`);
+                                selectedKey = key;
+                            }
                         }}
                     >
                         <Table.Cell>{key.key_type.toLowerCase()}</Table.Cell>
@@ -93,9 +91,13 @@
             </Empty.Header>
             <Empty.Content>
                 <div class="flex gap-2">
-
-                    <Button onclick={() => keygenDialogOpen = true}>new keypair</Button>
-                    <Button onclick={() => keyImportDialogOpen = true} variant="outline">import key</Button>
+                    <Button onclick={() => (keygenDialogOpen = true)}
+                        >new keypair</Button
+                    >
+                    <Button
+                        onclick={() => (keyImportDialogOpen = true)}
+                        variant="outline">import key</Button
+                    >
                 </div>
             </Empty.Content>
         </Empty.Root>
