57c0fe6088e90f7dbd34b8e35073200cd375ff06
te <thrzl@icloud.com>
2025-12-24 00:52:42 -0500

    feat: add basic badge

---
+++ src/components/VaultAuthStatus.svelte
@@ -0,0 +1,34 @@
+<script>
+    import Badge from "$lib/components/ui/badge/badge.svelte";
+    import Button from "$lib/components/ui/button/button.svelte";
+    import { EyeClosedIcon, EyeIcon } from "@lucide/svelte";
+    import { toast } from "svelte-sonner";
+    let { vaultUnlocked = $bindable(false) } = $props();
+</script>
+
+<div
+    class="position-absolute top-5 right-10 w-min"
+    id="vault-badge"
+    style="margin: 0"
+>
+    <Button
+        class="rounded-full py-1 line-height-none h-auto pointer-cursor"
+        variant={vaultUnlocked ? "default" : "outline"}
+        onclick={() => (vaultUnlocked = !vaultUnlocked)}
+    >
+        {#if vaultUnlocked}
+            <EyeIcon /> vault unlocked
+        {:else}
+            <EyeClosedIcon /> vault locked
+        {/if}
+    </Button>
+</div>
+
+<style>
+    #vault-badge {
+        position: fixed;
+        top: 5;
+        right: 5
+        width: min-content
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,18 +4,27 @@
     import { Toaster } from "$lib/components/ui/sonner";
     import * as Sidebar from "$lib/components/ui/sidebar/index.js";
     import AppSidebar from "../components/AppSidebar.svelte";
+    import VaultAuthStatus from "../components/VaultAuthStatus.svelte"
     import {page} from "$app/state"
     import { getCurrentWebviewWindow } from "@tauri-apps/api/webviewWindow";
     import { onMount } from "svelte";
+    import { listen } from "@tauri-apps/api/event";
+    import { commands } from "$lib/bindings";
 
     let { children } = $props();
+    let vaultUnlocked = $state(false);
+    listen("vault-unlock", () => {
+      vaultUnlocked = true
+    })
     onMount(async () => {
       let window = getCurrentWebviewWindow()
       await window.show()
+      vaultUnlocked = await commands.vaultUnlocked()
     })
 </script>
 
 <Toaster richColors />
+<VaultAuthStatus bind:vaultUnlocked />
 <Sidebar.Provider style="--sidebar-width: 12rem">
     {#if new URL(page.url).pathname !== "/create-vault"}<AppSidebar/>{/if}
 <div id="main-container" class="dark">
