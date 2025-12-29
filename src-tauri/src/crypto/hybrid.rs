9dc32e0c073d77af8e55c959444881a904b57e53
te <thrzl@icloud.com>
2025-12-29 01:25:26 -0500

    feat: implement `HybridRecipient::to_x25519`

--- src-tauri/src/crypto/hybrid.rs
+++ src-tauri/src/crypto/hybrid.rs
@@ -25,6 +25,46 @@ pub fn shake256<const N: usize>(input: &[u8]) -> [u8; N] {
     output
 }
 
+fn bech32_decode(string: &String) -> Result<Vec<u8>, String> {
+    if !string.starts_with(&"age1pq".to_string()) {
+        return Err("not a valid recipient".to_string());
+    }
+
+    let decoded = UncheckedHrpstring::new(string.as_str()).map_err(|e| e.to_string())?;
+
+    if decoded.data_part_ascii().len() < Bech32::CODE_LENGTH {
+        return Err("failed to parse string as Bech32".to_string());
+    }
+
+    {
+        // this is simply an implementation of UncheckedHrpString::validate_checksum
+        // it is reimplemented to exclude the length check
+        let mut checksum_eng = bech32::primitives::checksum::Engine::<Bech32>::new();
+        checksum_eng.input_hrp(decoded.hrp());
+
+        for fe in decoded
+            .data_part_ascii()
+            .iter()
+            .map(|&b| bech32::Fe32::from_char_unchecked(b))
+        {
+            checksum_eng.input_fe(fe);
+        }
+
+        if checksum_eng.residue() != &Bech32::TARGET_RESIDUE {
+            return Err("failed to decode as Bech32".to_string());
+        }
+    }
+
+    // strip checksum from data
+    let decoded =
+        &decoded.data_part_ascii()[..decoded.data_part_ascii().len() - Bech32::CHECKSUM_LENGTH];
+    Ok(decoded
+        .iter()
+        .map(|char| bech32::Fe32::from_char_unchecked(*char))
+        .fes_to_bytes()
+        .collect::<Vec<u8>>())
+}
+
 pub struct HybridRecipient {
     pub encapsulation_key: [u8; 1216],
 }
@@ -88,45 +128,22 @@ impl HybridRecipient {
         encoded_chars.collect()
     }
 
-    pub fn from_string(string: &String) -> Result<Self, String> {
-        if !string.starts_with(&"age1pq".to_string()) {
-            return Err("not a valid recipient".to_string());
-        }
-
-        let decoded = UncheckedHrpstring::new(string.as_str()).map_err(|e| e.to_string())?;
-
-        if decoded.data_part_ascii().len() < Bech32::CODE_LENGTH {
-            return Err("failed to parse string as Bech32".to_string());
-        }
-
-        {
-            // this is simply an implementation of UncheckedHrpString::validate_checksum
-            // it is reimplemented to exclude the length check
-            let mut checksum_eng = bech32::primitives::checksum::Engine::<Bech32>::new();
-            checksum_eng.input_hrp(decoded.hrp());
-
-            for fe in decoded
-                .data_part_ascii()
-                .iter()
-                .map(|&b| bech32::Fe32::from_char_unchecked(b))
-            {
-                checksum_eng.input_fe(fe);
-            }
-
-            if checksum_eng.residue() != &Bech32::TARGET_RESIDUE {
-                return Err("failed to decode as Bech32".to_string());
-            }
-        }
-
-        // strip checksum from data
-        let decoded =
-            &decoded.data_part_ascii()[..decoded.data_part_ascii().len() - Bech32::CHECKSUM_LENGTH];
-        let decoded_bytes = decoded
+    pub fn to_x25519(&self) -> age::x25519::Recipient {
+        let hrp = bech32::Hrp::parse_unchecked("age1pq");
+        let encoded_chars = self.encapsulation_key[1184..1216] // only the x25519 bit
             .iter()
-            .map(|char| bech32::Fe32::from_char_unchecked(*char))
-            .fes_to_bytes()
-            .collect::<Vec<u8>>();
+            .copied()
+            .bytes_to_fes()
+            .with_checksum::<Bech32>(&hrp)
+            .chars();
 
+        let age_key: String = encoded_chars.collect();
+        age::x25519::Recipient::from_str(age_key.as_str())
+            .expect("this should produce a valid x25519 recipient")
+    }
+
+    pub fn from_string(string: &String) -> Result<Self, String> {
+        let decoded_bytes = bech32_decode(string)?;
         Ok(Self {
             encapsulation_key: decoded_bytes.try_into().unwrap(),
         })
@@ -214,11 +231,11 @@ impl HybridIdentity {
     }
 
     /// parse an identity from a Bech32-encoded string
-    pub fn from_string(text: SecretString) -> Result<Self, String> {
-        let (hrp, decoded) = bech32::decode(text.expose_secret()).map_err(|e| e.to_string())?;
-        if hrp.as_str() != "AGE-SECRET-KEY-PQ-" {
+    pub fn from_string(string: SecretString) -> Result<Self, String> {
+        if !string.expose_secret().starts_with("AGE-SECRET-KEY-PQ-") {
             return Err("not a valid secret key".to_string());
         }
+        let decoded = bech32_decode(&string.expose_secret().to_string())?;
         Ok(Self {
             seed: SecretBox::from(Box::new(
                 decoded
@@ -231,13 +248,18 @@ impl HybridIdentity {
 
     pub fn to_x25519(&self) -> age::x25519::Identity {
         let (_, _, _, dk_t) = HybridIdentity::expand_key(&self.seed.expose_secret());
-        let hrp = bech32::Hrp::parse_unchecked("AGE-SECRET-KEY-");
-        age::x25519::Identity::from_str(
-            bech32::encode_upper::<bech32::Bech32>(hrp, dk_t.expose_secret())
-                .expect("failed to encode x25519 private key")
-                .as_str(),
-        )
-        .expect("x25519 private key was not valid")
+        let hrp = bech32::Hrp::parse_unchecked("age1pq");
+        let encoded_chars = dk_t
+            .expose_secret()
+            .iter()
+            .copied()
+            .bytes_to_fes()
+            .with_checksum::<Bech32>(&hrp)
+            .chars();
+
+        let identity_string: String = encoded_chars.collect::<String>().to_ascii_uppercase();
+        age::x25519::Identity::from_str(identity_string.as_str())
+            .expect("x25519 private key was not valid")
     }
 }
 
