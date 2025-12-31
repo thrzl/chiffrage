68775d26c405d5a8c36604c5f00f977c34c2951b
te <thrzl@icloud.com>
2025-12-30 20:15:47 -0500

    fix: don't close app on auth complete

--- src-tauri/src/store/commands.rs
+++ src-tauri/src/store/commands.rs
@@ -323,6 +323,7 @@ pub async fn authenticate(
 
         let password = SecretString::from(match rx.await {
             Ok(message) => {
+                println!("received message");
                 if message.len() == 0 {
                     return Ok(VaultStatusUpdate::AuthenticationCancel);
                 }
@@ -331,6 +332,7 @@ pub async fn authenticate(
             }
             Err(error) => return Err(error.to_string()),
         });
+        println!("setting vault key");
         let unlock_attempt = state.with_vault(|vault| vault.set_vault_key(password))?;
         if let Err(error) = unlock_attempt {
             if error.as_str() == "integrity check failed" {
@@ -342,6 +344,7 @@ pub async fn authenticate(
             break;
         };
     }
+    println!("about to return");
     if integrity_check_fail {
         let _ = regenerate_public_identities(state).await;
     }
@@ -351,7 +354,6 @@ pub async fn authenticate(
         VaultStatusUpdate::Unlocked
     };
     let _ = app_handle.emit("vault-status-update", &result);
-    let _ = webview.close();
 
     Ok(result)
 }
